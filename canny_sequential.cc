69e66261df8dcc4f116a3ff87d34885e415ab222
yvetteyuanqin <lilyoorainqy@gmail.com>
2018-02-23 11:08:35 -0800

    save2

--- canny_sequential.cc
+++ canny_sequential.cc
@@ -28,13 +28,22 @@ void png_read(string &filename){
     imgInfo fingerprint;
     Image img;
     img.read(filename);
-    fingerprint.height = img.columns();
-    fingerprint.width = img.colums();
+    fingerprint.height = img.rows();
+    fingerprint.width = img.columns();
     fingerprint.depth = img.depth();
+    double *pixels = new double[width*height];
+    image.write(0,0,width,height,"",DoublePixel, pixels);
+    for(int i =0; i<width, i++){
+        for(j=0;j<height,j++){
+            cout << pixels[i][j];
+        }
+        cout << endl;
+    }
+    
 }
 
 void main(){
     string *filename="001.png";
     png_read(filename);
-    print()
+
 }
