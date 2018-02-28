56dbf8422f26dbbe54bfe0b80214da6ab5ae5434
yvetteyuanqin <lilyoorainqy@gmail.com>
2018-02-27 18:52:43 -0800

    try2

--- readtest.cc
+++ readtest.cc
@@ -20,17 +20,27 @@ int main() {
     gray8_image_t img;
     png_read_image("001.png", img);
     png_write_view("testimg.png",const_view(img));
-
+    
 
 	//image view
-	image_view img_view(img);
-	for (int i = 0; i < img_view.height(); i++) {
-		for (int j = 0; j < img_view.width(); j++) {
-			cout << img_view.xy_at(j, i);
-		}
-		cout << endl;
-	}
+//	image_view img_view(img);
+    gil::gray8_image_t::const_view_t img_view = const_view(img);
+    
+    assert(img_view.is_1d_traversable());
+    
+    int width = img_view.width();
+    int height = img_view.height();
+    const char* buffer = img_view.begin().x();
+    
+    cout<< *buff;
+    
+//    for (int i = 0; i < img_view.height(); i++) {
+//        for (int j = 0; j < img_view.width(); j++) {
+//            cout << img_view;
+//        }
+//        cout << endl;
+//    }
 
-
+    //
 
 }
