86899bed96a7e8853e7776a103801e9255ef3ec8
yvetteyuanqin <lilyoorainqy@gmail.com>
2018-02-27 19:15:05 -0800

    try yuan

--- readtest.cc
+++ readtest.cc
@@ -1,8 +1,8 @@
 /**
-*  Sequential canny detection
-*
-*
-*/
+ *  Sequential canny detection
+ *
+ *
+ */
 //Yuan Qin
 //Lai Man Tang
 
@@ -15,24 +15,35 @@
 using namespace std;
 
 int main() {
-	
+    
     using namespace boost::gil;
     gray8_image_t img;
     png_read_image("001.png", img);
     png_write_view("testimg.png",const_view(img));
     
-
-    //image view
-    image_view img_view(img);
-
     
-    for (int i = 0; i < img_view.height(); i++) {
-        for (int j = 0; j < img_view.width(); j++) {
-            cout << img_view.xy_at(j,i);
-        }
-        cout << endl;
+    
+    
+    unsigned char * buf = new unsigned char[w * h];
+    
+    gray8_view_t image = interleaved_view(w, h, (gray8_pixel_t*)buf, w);
+    
+    for (size_t i = 0; i < ...; ++i)
+    {
+        boost::gil::gray8_view_t::x_iterator it = image.row_begin(i);
+        
+        // use it[j] to access pixel[i][j]
     }
-
-    //
-
+    
+    
+    //    for (int i = 0; i < img_view.height(); i++) {
+    //        for (int j = 0; j < img_view.width(); j++) {
+    //            cout << img_view;
+    //        }
+    //        cout << endl;
+    //    }
+    
+    
+    
 }
+
