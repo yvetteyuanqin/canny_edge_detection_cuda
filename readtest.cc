fe11369d232148c2b30fd1598dcd98a2917c5037
yvetteyuanqin <lilyoorainqy@gmail.com>
2018-02-27 18:26:46 -0800

    try1

--- readtest.cc
+++ readtest.cc
@@ -11,27 +11,20 @@
 #include <boost/gil/extension/io/png_dynamic_io.hpp>
 #include <stdint.h>
 #include <vector>
+#include <iostream>
 
-struct PixelInserter {
-	std::vector<uint8_t>* storage;
-	PixelInserter(std::vector<uint8_t>* s) : storage(s) {}
-	void operator()(boost::gil::gray8_pixel_t p) const {
-		storage->push_back(boost::gil::at_c<0>(p));
-		//        storage->push_back(boost::gil::at_c<1>(p));
-		//        storage->push_back(boost::gil::at_c<2>(p));
-	}
-};
 
 int main() {
-	std::vector<uint8_t> storage;
-	{
-		using namespace boost::gil;
-		gray8_image_t img;
-		png_read_image("001.png", img);
-        //gil::rgb8_image_t img(height, width);
-        png_write_view("testimg.png",const_view(img));
-		//storage.reserve(img.width() * img.height() * num_channels<rgb8_image_t>());
-		//for_each_pixel(const_view(img), PixelInserter(&storage));
-	}
+	
+    using namespace boost::gil;
+    gray8_image_t img;
+    png_read_image("001.png", img);
+    png_write_view("testimg.png",const_view(img));
+    for(int i=0;i<img.height();i++){
+        for(int j =0;j<img.width();j++)
+            cout<<img[i][j];
+        cout<<endl;
+    }
+    
 
 }
