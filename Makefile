2bd13d3878633434e4d5fd1173cc9ebb228fd864
littlecloud1 <cloudtang030@gmail.com>
2018-02-27 19:01:46 -0800

    branch

--- Makefile
+++ Makefile
@@ -9,6 +9,9 @@ readtest: readtest.cc
 	g++ -std=c++11 -O3 -g -lpng -I/data/apps/boost/1.57/include -o readtest readtest.cc
 
 
+readtest2: readtest_2.cc 
+	g++ -std=c++11 -O3 -g -lpng -I/data/apps/boost/1.57/include -o readtest_2 readtest_2.cc
+
 
 clean:
 	rm -f
---
+++ readtest_2.cc
@@ -0,0 +1,38 @@
+/**
+*  Sequential canny detection
+*
+*
+*/
+//Yuan Qin
+//Lai Man Tang
+
+
+#include <boost/gil/rgb.hpp>
+#include <boost/gil/extension/io/png_dynamic_io.hpp>
+#include <stdint.h>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+    using namespace boost::gil;
+    gray8_image_t img;
+    png_read_image("001.png", img);
+    png_write_view("testimg.png",const_view(img));
+    
+
+    image view
+    image_view img_view(img);
+
+    
+    for (int i = 0; i < img_view.height(); i++) {
+        for (int j = 0; j < img_view.width(); j++) {
+            cout << img_view;
+        }
+        cout << endl;
+    }
+
+    //
+
+}
