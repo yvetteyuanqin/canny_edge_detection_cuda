22b207eae53bea3bf3079ddfe2ab812fb5b6a98c
yvetteyuanqin <lilyoorainqy@gmail.com>
2018-03-01 15:54:08 -0800

    a

--- readtest.cc
+++ readtest.cc
@@ -40,6 +40,7 @@ int main() {
         for (int j = 0; j < WIDTH; ++j) {
             imgbuff[i][j]=srcview(j, i);
             cout<<hex<<int(imgbuff[i][j]);
+            cnt += 1;
         }
     }
 //    for (size_t i = 0; i < img.height(); ++i)
