7c727e01b8438fd810d33ae17d3ba7fcd5693325
yvetteyuanqin <lilyoorainqy@gmail.com>
2018-03-01 19:26:35 -0800

    ifndef

--- canny_sequential.h
+++ canny_sequential.h
@@ -1,3 +1,5 @@
+#ifndef _CANNY_SEQUENTIAL_HPP_
+#define _CANNY_SEQUENTIAL_HPP_
 #include <boost/gil/rgb.hpp>
 #include <boost/gil/extension/io/png_dynamic_io.hpp>
 #include <stdint.h>
@@ -17,4 +19,4 @@ Matrix createKernel(int height, int width, double sigma);
 /*Step 1 blur the image to reduce noice*/
 void gaussian_filter(gray8_pixel_t **newImage,gray8_pixel_t **in_pixels,int width, int height);
 
-
+#endif
