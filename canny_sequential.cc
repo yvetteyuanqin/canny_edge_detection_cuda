b584f6937bbb94f913a29b89232d18c0203a27a5
yvetteyuanqin <lilyoorainqy@gmail.com>
2018-03-03 14:08:39 -0800

    format pixel8

--- canny_sequential.cc
+++ canny_sequential.cc
@@ -53,12 +53,12 @@ void gaussian_filter(gray8_pixel_t **newImage,gray8_pixel_t **in_pixels,int widt
 
     int i,j,h,w;
     /*allocate newimage*/
-    filter[0][0]=1, filter[0][1]=4, filter[0][2]=7, filter[0][3]=4, filter[0][4]=1,
-    filter[1][0]=4, filter[1][1]=16, filter[1][2]=26, filter[1][3]=16, filter[1][4]=4,
-    filter[2][0]=7, filter[2][1]=26, filter[2][2]=41, filter[2][3]=26, filter[2][4]=7,
-    filter[3][0]=4, filter[3][1]=16, filter[3][2]=26, filter[3][3]=16, filter[3][4]=4,
-    filter[4][0]=1, filter[4][1]=4, filter[4][2]=7, filter[4][3]=4, filter[4][4]=1;
-    
+//    filter[0][0]=1, filter[0][1]=4, filter[0][2]=7, filter[0][3]=4, filter[0][4]=1,
+//    filter[1][0]=4, filter[1][1]=16, filter[1][2]=26, filter[1][3]=16, filter[1][4]=4,
+//    filter[2][0]=7, filter[2][1]=26, filter[2][2]=41, filter[2][3]=26, filter[2][4]=7,
+//    filter[3][0]=4, filter[3][1]=16, filter[3][2]=26, filter[3][3]=16, filter[3][4]=4,
+//    filter[4][0]=1, filter[4][1]=4, filter[4][2]=7, filter[4][3]=4, filter[4][4]=1;
+//
 //    for (int i=0 ; i<height ; i++) {
 //        for (int j=0 ; j<width ; j++) {
 //            filter[i][j] /= 273;
@@ -71,7 +71,7 @@ void gaussian_filter(gray8_pixel_t **newImage,gray8_pixel_t **in_pixels,int widt
 				cout << j << " ";
                 for (h=i ; h<i+filterHeight ; h++) {
                     for (w=j ; w<j+filterWidth ; w++) {
-                        newImage[i][j] = newImage[i][j] +filter[h-i][w-j]*in_pixels[h][w];
+                        newImage[i][j] = newImage[i][j] +gray8_pixel_t(filter[h-i][w-j]*char(in_pixels[h][w]));
                     }
                 }
             }
