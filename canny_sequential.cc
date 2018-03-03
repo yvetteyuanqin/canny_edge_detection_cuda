553954c17ea63dfa9ba2a3e61222e0eb50073ef7
yvetteyuanqin <lilyoorainqy@gmail.com>
2018-03-03 14:37:17 -0800

    try pointer

--- canny_sequential.cc
+++ canny_sequential.cc
@@ -66,7 +66,7 @@ void gaussian_filter(gray8_pixel_t **newImage,gray8_pixel_t **in_pixels,int widt
 				cout << j << " ";
                 for (h=i ; h<i+filterHeight ; h++) {
                     for (w=j ; w<j+filterWidth ; w++) {
-                        newImage[i][j] = newImage[i][j] +gray8_pixel_t(filter[h-i][w-j]*char(in_pixels[h][w]));
+                        *（*（newImage+i）+j） = *（*（newImage+i）+j） +gray8_pixel_t(filter[h-i][w-j]*char(*(*(in_pixels+h)+w)));
                     }
                 }
             }
