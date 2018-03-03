f5c868b31dcfa7ae0cc73de1b79cf2b99d6f2295
yvetteyuanqin <lilyoorainqy@gmail.com>
2018-03-03 14:04:09 -0800

    try filter

--- canny_sequential.cc
+++ canny_sequential.cc
@@ -34,6 +34,7 @@ Matrix createKernel(int height, int width, double sigma)
     return kernel;
 }
 
+
 /*Step 1 blur the image to reduce noice*/
 void gaussian_filter(gray8_pixel_t **newImage,gray8_pixel_t **in_pixels,int width, int height)
 {
@@ -43,8 +44,8 @@ void gaussian_filter(gray8_pixel_t **newImage,gray8_pixel_t **in_pixels,int widt
 	cout << "finish create kernel" << endl;
     int filterHeight = filter.size();
     int filterWidth = filter[0].size();
-    int newImageHeight = height-filterHeight;
-    int newImageWidth = width-filterWidth;
+    int newImageHeight = height-filterHeight+1;
+    int newImageWidth = width-filterWidth+1;
 
 	cout << "newImageHeight " << newImageHeight << " newImageWidth " << newImageWidth << endl;
 	cout << "filterHeight " << filterHeight  << " filterWidth " << filterWidth << endl;
@@ -52,7 +53,17 @@ void gaussian_filter(gray8_pixel_t **newImage,gray8_pixel_t **in_pixels,int widt
 
     int i,j,h,w;
     /*allocate newimage*/
+    filter[0][0]=1, filter[0][1]=4, filter[0][2]=7, filter[0][3]=4, filter[0][4]=1,
+    filter[1][0]=4, filter[1][1]=16, filter[1][2]=26, filter[1][3]=16, filter[1][4]=4
+    filter[2][0]=7, filter[2][1]=26, filter[2][2]=41, filter[2][3]=26, filter[2][4]=7
+    filter[3][0]=4, filter[3][1]=16, filter[3][2]=26, filter[3][3]=16, filter[3][4]=4
+    filter[4][0]=1, filter[4][1]=4, filter[4][2]=7, filter[4][3]=4, filter[4][4]=1
     
+    for (int i=0 ; i<height ; i++) {
+        for (int j=0 ; j<width ; j++) {
+            filter[i][j] /= 273;
+        }
+    }
 	cout << "start convolution" << endl;
         for (i=0 ; i<newImageHeight ; i++) {
 			
