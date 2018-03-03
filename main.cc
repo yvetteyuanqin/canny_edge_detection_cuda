643b70eac99f6ffa5ededa64b32147d78c683e2f
yvetteyuanqin <lilyoorainqy@gmail.com>
2018-03-03 15:58:53 -0800

    A

--- main.cc
+++ main.cc
@@ -36,6 +36,8 @@ int main() {
         *(imgbuff+i) = (gray8_pixel_t*)malloc(sizeof(gray8_pixel_t)*HEIGHT);
     }
     
+    
+    
     gray8_view_t srcview = interleaved_view(img.width(), img.height(), (gray8_pixel_t*)buf, view(img).pixels().row_size());
     int cnt = 0;
     for (int i = 0; i < HEIGHT; ++i) {
@@ -45,16 +47,26 @@ int main() {
            
         } cnt += 1;
     }
+    //1d image buffer
+    gray8_pixel_t *imgbuff1d = new gray8_pixel_t[HEIGHT*WIDTH];
+    for (int i = 0; i < HEIGHT; ++i) {
+        for (int j = 0; j < WIDTH; ++j) {
+            imgbuff1d[i*WIDTH+j]=srcview(j, i);
+            // cout<<hex<<int(imgbuff[i][j]);
+            
+        } 
+    }
+
 
     /*canny_sequential*/
-    gray8_pixel_t **newImage = (gray8_pixel_t**)malloc(sizeof(gray8_pixel_t*)*512);
+    gray8_pixel_t **newImage = (gray8_pixel_t**)malloc(sizeof(gray8_pixel_t*)*508);
     for(int i = 0; i < WIDTH; i++)
     {
-        *(imgbuff+i) = (gray8_pixel_t*)malloc(sizeof(gray8_pixel_t)*512);
+        *(imgbuff+i) = (gray8_pixel_t*)malloc(sizeof(gray8_pixel_t)*508);
         for(int j = 0;j<HEIGHT;j++) imgbuff[i][j]=0;
     }
-    //1D buffer
-    
+    //1D newimagebuffer
+    gray8_pixel_t *newImage1d = new gray8_pixel_t[HEIGHT*WIDTH];
     
      cout<<endl;
      cout<<cnt<<endl;
@@ -62,9 +74,16 @@ int main() {
      
     
     /*apply gaussian filter*/
-	cout << "enter gaussian filter" << endl;
-    gaussian_filter(newImage,imgbuff,WIDTH, HEIGHT);
-	cout << "finished." << endl;
+    //2D
+//    cout << "enter gaussian filter" << endl;
+//    gaussian_filter(newImage,imgbuff,WIDTH, HEIGHT);
+//    cout << "finished." << endl;
+    //1D
+    /*apply gaussian filter*/
+    cout << "enter gaussian filter" << endl;
+    gaussian_filter(newImage1d,imgbuff1d,WIDTH, HEIGHT);
+    cout << "finished." << endl;
+    
     
     /*revert image to check*/
     gray8_image_t img2(HEIGHT,WIDTH);
