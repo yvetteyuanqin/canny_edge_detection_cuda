1ab959c492f93c130b5a7383cdb0971102d338be
yvetteyuanqin <lilyoorainqy@gmail.com>
2018-03-05 14:43:32 -0800

    correct index

--- canny_sequential.cc
+++ canny_sequential.cc
@@ -230,9 +230,9 @@ void apply_hysteresis(gray8_pixel_t **out_pixels, gray8_pixel_t **in_pixels, gra
                     
                     /* check 8 immediately surrounding neighbors
                      * if any of the neighbors are above the low threshold, preserve edge */
-                    trace_immed_neighbors(out_pixels, in_pixels, t, t_low);
+                    trace_immed_neighbors(out_pixels, in_pixels, i,j, t_low);
                 } else {
-                    out_pixels[t] = 0x00;
+                    out_pixels[i][j] = 0x00;
                 }
             }
         }
