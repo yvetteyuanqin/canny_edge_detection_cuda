3e1aed923bc43e543f9d01b40866c13b54b19ce2
yvetteyuanqin <lilyoorainqy@gmail.com>
2018-03-08 20:31:25 -0800

    a

--- main_p.cu
+++ main_p.cu
@@ -175,7 +175,7 @@ cout << "finished." << endl;
 
 /*revert image to check*/
 gray8_image_t img2(HEIGHT,WIDTH);
-gray8c_view_t img_view = view(img2);
+const View &img_view = view(img2);
 
 cout << "create image view" << endl;
 for (int i = 0; i < HEIGHT; ++i) {
