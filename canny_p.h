6550cafb7997fb297fe9ef31f2ec221f4817bf4c
Lai Man Tang <cloudtang030@gmail.com>
2018-03-10 20:29:09 -0800

    move edge_detector postition

--- canny_p.h
+++ canny_p.h
@@ -10,6 +10,10 @@
 typedef std::vector<double> Array;
 typedef std::vector<Array> Matrix;
 
+
+void edge_detector(unsigned char** h_newImg, unsigned char** h_imgbuff, int WIDTH, int HEIGHT);
+
+
 /*create a gaussian filter*/
 Matrix createKernel(int height, int width, double sigma);
 /*Step 1 blur the image to reduce noice*/
@@ -21,5 +25,4 @@ void suppress(unsigned char **newImage, unsigned char **mag, int width, int heig
 void apply_hysteresis(unsigned char **out_pixels, unsigned char **in_pixels, unsigned  char t_high, unsigned  char t_low, int width,int height);
 void trace_immed_neighbors(unsigned char **out_pixels, unsigned char **in_pixels, unsigned i, unsigned j, unsigned  char t_low);
 
-void edge_detector((unsigned char)** h_newImg, (unsigned char)** h_imgbuff, int WIDTH, int HEIGHT);
 #endif
