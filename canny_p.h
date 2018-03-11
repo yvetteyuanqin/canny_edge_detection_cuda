4370bd9e39904b2faabdcada6c870a33b621f5ce
Lai Man Tang <cloudtang030@gmail.com>
2018-03-10 20:40:11 -0800

    tags

--- canny_p.h
+++ canny_p.h
@@ -16,14 +16,14 @@ void edge_detector(unsigned char** h_newImg, unsigned char** h_imgbuff, int WIDT
 
 
 /*create a gaussian filter*/
-Matrix createKernel(int height, int width, double sigma);
+__device__ Matrix createKernel(int height, int width, double sigma);
 /*Step 1 blur the image to reduce noice*/
-void gaussian_filter(unsigned char**newImage, unsigned char **in_pixels,int width, int height);
-void gradient(unsigned char **newImage, unsigned char **mag, int width, int height,
+__global__ void gaussian_filter(unsigned char**newImage, unsigned char **in_pixels,int width, int height);
+__global__ void gradient(unsigned char **newImage, unsigned char **mag, int width, int height,
 	unsigned char **deltaX, unsigned char **deltaY);
-void suppress(unsigned char **newImage, unsigned char **mag, int width, int height,
+__global__ void suppress(unsigned char **newImage, unsigned char **mag, int width, int height,
 	unsigned char **deltaX, unsigned char **deltaY);
-void apply_hysteresis(unsigned char **out_pixels, unsigned char **in_pixels, unsigned  char t_high, unsigned  char t_low, int width,int height);
-void trace_immed_neighbors(unsigned char **out_pixels, unsigned char **in_pixels, unsigned i, unsigned j, unsigned  char t_low);
+__global__ void apply_hysteresis(unsigned char **out_pixels, unsigned char **in_pixels, unsigned  char t_high, unsigned  char t_low, int width,int height);
+__global__ void trace_immed_neighbors(unsigned char **out_pixels, unsigned char **in_pixels, unsigned i, unsigned j, unsigned  char t_low);
 
 #endif
