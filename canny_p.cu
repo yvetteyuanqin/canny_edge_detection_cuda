814219f6fc6d4ebd8cbaee442ef872a61a1d17b0
littlecloud1 <cloudtang030@gmail.com>
2018-03-12 15:08:22 -0700

    (void**)

--- canny_p.cu
+++ canny_p.cu
@@ -320,16 +320,16 @@ void edge_detector(unsigned char** h_newImg, unsigned char** h_imgbuff, int WIDT
 	//stopwatch_init();
 	//timer = stopwatch_create();
 
-	unsigned char **d_imgbuff;
-	unsigned char **d_newImage;
+	unsigned char *d_imgbuff;
+	unsigned char *d_newImage;
 	cout<<"cudaMalloc"<<endl;
 	cudaMalloc((void**)&d_imgbuff, sizeof(unsigned char*)*HEIGHT);
 	cudaMalloc((void**)&d_newImage, sizeof(unsigned char*)*HEIGHT);
 	printf("cuda1D finish");
 	for (int i = 0; i < WIDTH; i++)
 	{
-		cudaMalloc(&d_imgbuff[i], sizeof(unsigned char)*WIDTH);
-		cudaMalloc(&d_newImage[i], sizeof(unsigned char)*WIDTH);
+		cudaMalloc((void**)&d_imgbuff[i], sizeof(unsigned char)*WIDTH);
+		cudaMalloc((void**)&d_newImage[i], sizeof(unsigned char)*WIDTH);
 	}
 	//memcopy
 	printf("cuda2D finish");
