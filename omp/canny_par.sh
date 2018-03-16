3a14f2fa33d085379ab81e150ce36b66afa1fdfb
littlecloud1 <cloudtang030@gmail.com>
2018-03-15 20:56:46 -0700

    sh

--- omp/canny_par.sh
+++ omp/canny_par.sh
@@ -5,9 +5,7 @@
 
 # Module load gcc compiler version 4.8.2
 module load  gcc/4.9.2
-# Module load Cuda Compilers and GCC
-module load  cuda/5.0
-module load  gcc/4.4.3
+
 
 # Runs a bunch of standard command-line
 # utilities, just as an example:
